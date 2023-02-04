b9c43e245048353327ad0cc7678460c0f67669a3
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-02-04 22:21:21 +0100

    HUB-juegos

--- pages/API/API.js
+++ pages/API/API.js
@@ -1,70 +1,99 @@
-import "./API.css";
+//He rehecho todo el proceso de la API despues de llegar a un punto muerto que me estaba quitando mcho tiempo.
+
+import ("./API.css")
 
 const template = () => `
 <a href="./Home/Home" class="back">BACK</a>
-<section class="API">
-  <h2>API Pokemon</h2>
-    <ul>
-      <input type="text" id="logInput"/>
-      <a class="pokeball" id="loginBtn"><img src="/public/utils/287221.png" alt""></a>
-    </ul>
-      <div class="pokemon"></div>
+<h2>PokeApi</h2>
+  <div class="pokemon">
+    <div class="Div"><input type="text" id="search" class="input" placeholder="Busca tu pokemon"/></div>
+    <nav id="navBar" class="nav"></nav>
+    <section id="gallery" class="gallery">
 </section>
-`;
+  </div>
+`
 
-let pokeArray = []
-const pokemon = async () => {
-  for (let i = 1; i <= 151; i++){
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-  const data = await res.json();
-  pokeArray.push(data);
-};
-mapPokemon(pokeArray);
+const Url = "https://pokeapi.co/api/v2/pokemon/"
+let index = 1;
+let pokemons = []
+const getCharacters = async () => {
+    for (let i = 1; i <= 151; i++){
+        const data = await fetch(`${Url}${index}`)
+        index++
+        const json = await data.json();
+        pokemons.push(json);
+    }
+    mapPokemons(pokemons)
 }
 
-const mapPokemon = (pokemons) => {
-  let allPokemon = pokemons.map((pokemon) => ({
+let mapPokemon = []
+const mapPokemons = () =>{
+ mapPokemon = pokemons.map((pokemon) =>({
     image: pokemon.sprites.other.dream_world.front_default,
+    id: pokemon.id,
     name: pokemon.name,
     experience: pokemon.base_experience,
-    element: pokemon.types[0].type.name,
-    weight: pokemon.weight
-   }));
-   printPokemons(allPokemon);
- };
+    height: (pokemon.height)*10,
+    weight: (pokemon.weight) /10,
+    type: pokemon.types[0].type.name,
+    type2: pokemon.types[1]?.type.name,
+})) 
+printPokemons(mapPokemon)
+}
 
- const printPokemons = (pokemons) => {
-  const container = document.querySelector(".pokemon");
-  container.innerHTML = " ";
+const printPokemons = (pokemons) => {
+  const gallery = document.querySelector("#gallery");
+  gallery.innerHTML = "";
   for (const pokemon of pokemons){
-    const div = document.createElement("figure");
-    div.innerHTML = `
-    <h2>${pokemon.name}</h2>
-    <img src=${pokemon.image} alt= ${pokemon.name}
-    <h3>${pokemon.experience}</h3>
-    <h3>${pokemon.element}</h3>
-    <h3>${pokemon.weight}</h3>
-    `;
-    container.appendChild(div);
-  }
- }
-
- const filterCharacters = (characters) => {
-  const logInput = document.querySelector("#logInput");
-  const filteredCharacters = characters.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(logInput.value.toLowerCase())
-  );
-  printPokemons(filteredCharacters);
-};
+      const figure = document.createElement("figure");
+      const card = document.createElement("div")
+      figure.innerHTML = `
+      <h3>${pokemon.id}</h3>
+      <h2>${pokemon.name}</h2>
+      <img src=${pokemon.image} alt= ${pokemon.name}
+      <h3>${pokemon.experience}</h3>
+      <h3 class=${pokemon.type}>${pokemon.type}</h3>
+      <h3 class=${pokemon.type2}>${pokemon?.type2}</h3>
+      <h3>${pokemon.height}</h3>
+      <h3>${pokemon.weight}</h3>
+      `
+    card.appendChild(figure);
+    gallery.appendChild(card);
+  }}
 
 const addListener = () => {
-  logInput.addEventListener("input", () => {
-    document.querySelector(".pokemon").innerHTML = " ";
-    filterCharacters("allPokemon");
-  });}
+const search = document.querySelector("#search")
+search.addEventListener("input", () => {
+  filterCharacters(mapPokemon)
+})
+} 
+   
+const filterCharacters = (pokemons) => {
+  const filter = pokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(search.value.toLowerCase()))
+    printPokemons(filter);
+}
+
+const categories = ["grass", "poison", "fire", "flying", "water", "bug", "normal", "electric", "ground", "fairy", "fighting", "psychic", "rock", "steel", "ice", "ghost", "dragon"]; 
+const getButtons = () =>{
+const button = document.querySelector("#navBar")
+var coincidencia
+for (const type of categories){
+  const Btn = document.createElement("button")
+  Btn.innerText = type
+  Btn.classList.add(type)
+  button.appendChild(Btn)
+  Btn.addEventListener("click", (ev) =>{  
+    
+    coincidencia = mapPokemon.filter((pokemon) => {
+      return pokemon.type === ev.target.innerHTML 
+    });
+  })
+  console.log(coincidencia);
+}}
 
 export const printTemplate = () => {
   document.querySelector("#app").innerHTML = template();
+  getCharacters();
   addListener();
-  pokemon();
-};
+  getButtons();
+}
